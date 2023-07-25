602a78a67aaf6dea704e23825948998950b0c1c8
gaeng2y <gaeng2y@gmail.com>
2023-07-26 00:10:49 +0900

    🎉 init project

---
+++ SwiftMacroSample/SwiftMacroSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftMacroSample
+//
+//  Created by Kyeongmo Yang on 2023/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftMacroSample/SwiftMacroSample/SwiftMacroSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMacroSampleApp.swift
+//  SwiftMacroSample
+//
+//  Created by Kyeongmo Yang on 2023/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMacroSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
